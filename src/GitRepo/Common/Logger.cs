df3db5e3b2251dfbd67827fc97bcdb27af4784cf
Pavel Simsa <pavel@simsa.cz>
2025-01-27 20:25:43 +0100

    Fix recursion

--- src/GitRepo/Common/Logger.cs
+++ src/GitRepo/Common/Logger.cs
@@ -4,5 +4,5 @@ internal static class Logger
 {
     public static void Log() => Console.WriteLine();
 
-    public static void Log(string message) => Logger.Log(message);
+    public static void Log(string message) => Console.WriteLine(message);
 }
