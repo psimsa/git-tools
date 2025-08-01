50fe4633ea3d1bdf1cc21fd6a1ede492d958d50d
Pavel Simsa <pavel@simsa.cz>
2025-08-01 15:27:16 +0000

    Update

--- src/GitRepo/Program.cs
+++ src/GitRepo/Program.cs
@@ -18,21 +18,21 @@ return await result.InvokeAsync();
 static void SetupNukeCommand(RootCommand rootCommand, Option<bool> debugOption)
 {
     var nukeCommand = new Command("nuke", "Clean-up current repo");
-    var quietOption = new Option<bool>("--quiet");
-    quietOption.Aliases.Add("-q");
-    quietOption.Description = "Do not ask for confirmation";
-    var noSwitchBranchOption = new Option<bool>("--no-switch-branch");
-    noSwitchBranchOption.Aliases.Add("-n");
-    noSwitchBranchOption.Description = "Do not switch to main or master branch";
-    var useBranchOption = new Option<string?>("--use-branch");
-    useBranchOption.Aliases.Add("-b");
-    useBranchOption.Description = "Branch to use instead of master/main branch";
     nukeCommand.Add(debugOption);
+
+    var quietOption = new Option<bool>("--quiet", ["-q"]);
+    quietOption.Description = "Do not ask for confirmation";
     nukeCommand.Add(quietOption);
+
+    var noSwitchBranchOption = new Option<bool>("--no-switch-branch", ["-n"]);
+    noSwitchBranchOption.Description = "Do not switch to main or master branch";
     nukeCommand.Add(noSwitchBranchOption);
+
+    var useBranchOption = new Option<string?>("--use-branch", ["-b"]);
+    useBranchOption.Description = "Branch to use instead of master/main branch";
     nukeCommand.Add(useBranchOption);
 
-    nukeCommand.SetAction(async (System.CommandLine.ParseResult parseResult) =>
+    nukeCommand.SetAction(async (ParseResult parseResult) =>
         await NukeCommand.Run(
             parseResult.GetValue(debugOption),
             parseResult.GetValue(quietOption),
@@ -48,17 +48,17 @@ static void SetupTidyBranchCommand(RootCommand rootCommand, Option<bool> debugOp
         "tidy-branch",
         "Create single squashed commit from current branch rebased onto another branch (master/main by default)"
     );
-    var quietOption = new Option<bool>("--quiet");
-    quietOption.Aliases.Add("-q");
-    quietOption.Description = "Do not ask for confirmation";
-    var targetBranchOption = new Option<string?>("--target-branch");
-    targetBranchOption.Aliases.Add("-t");
-    targetBranchOption.Description = "Branch to rebase onto";
     tidyBranchCommand.Add(debugOption);
+
+    var quietOption = new Option<bool>("--quiet", ["-q"]);
+    quietOption.Description = "Do not ask for confirmation";
     tidyBranchCommand.Add(quietOption);
+
+    var targetBranchOption = new Option<string?>("--target-branch", ["-t"]);
+    targetBranchOption.Description = "Branch to rebase onto";
     tidyBranchCommand.Add(targetBranchOption);
 
-    tidyBranchCommand.SetAction(async (System.CommandLine.ParseResult parseResult) =>
+    tidyBranchCommand.SetAction(async (ParseResult parseResult) =>
         await TidyBranchCommand.Run(
             parseResult.GetValue(debugOption),
             parseResult.GetValue(quietOption),
@@ -70,21 +70,22 @@ static void SetupTidyBranchCommand(RootCommand rootCommand, Option<bool> debugOp
 static void SetupBootstrapCommand(RootCommand rootCommand, Option<bool> debugOption)
 {
     var bootstrapCommand = new Command("bootstrap", "Bootstrap repo from parameters");
-    var templateOption = new Option<string>("--template");
-    templateOption.Aliases.Add("-t");
-    templateOption.Description = "Template to use";
-    var defaultBranchOption = new Option<string>("--default-branch");
-    defaultBranchOption.Aliases.Add("-b");
-    defaultBranchOption.Description = "Default branch to use";
-    var userEmailOption = new Option<string>("--user-email");
-    userEmailOption.Aliases.Add("-e");
-    userEmailOption.Description = "User email to use";
     bootstrapCommand.Add(debugOption);
+
+    var templateOption = new Option<string>("--template", ["-t"]);
+    templateOption.Description = "Template to use";
+    templateOption.Hidden = true; // Hide this option from the help output because it's not implemented yet
     bootstrapCommand.Add(templateOption);
+
+    var defaultBranchOption = new Option<string>("--default-branch", ["-b"]);
+    defaultBranchOption.Description = "Default branch to use";
     bootstrapCommand.Add(defaultBranchOption);
+
+    var userEmailOption = new Option<string>("--user-email", ["-e"]);
+    userEmailOption.Description = "User email to use";
     bootstrapCommand.Add(userEmailOption);
 
-    bootstrapCommand.SetAction(async (System.CommandLine.ParseResult parseResult) =>
+    bootstrapCommand.SetAction(async (ParseResult parseResult) =>
         await BootstrapCommand.Run(
             parseResult.GetValue(debugOption),
             parseResult.GetValue(templateOption),
